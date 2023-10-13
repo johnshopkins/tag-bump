091300d6d89e07c8bcaab2b083f04b53043e84c6
Jen Wachter <jwachter@jhu.edu>
2023-10-13 09:34:41 -0400

    add all parts of the tag (major, minor, bugfix) even if they aren't present on the existing git tags.

--- test/lib/get-latest-tag.test.js
+++ test/lib/get-latest-tag.test.js
@@ -3,15 +3,15 @@ import getLatestTag from '../../lib/get-latest-tag.js';
 describe('getLatestTag',  () => {
 
   test('No tags',  () => {
-    expect(getLatestTag([])).toEqual('0');
+    expect(getLatestTag([])).toEqual('0.0.0');
   });
 
   test('Major version without a minor version',  () => {
-    expect(getLatestTag(['1.0', '2'])).toEqual('2.0');
+    expect(getLatestTag(['1.0', '2'])).toEqual('2.0.0');
   });
 
   test('Major.minor version without a bugfix version',  () => {
-    expect(getLatestTag(['1.0', '2.1'])).toEqual('2.1');
+    expect(getLatestTag(['1.0', '2.1'])).toEqual('2.1.0');
   });
 
   test('Major.minor version with multiple bugfix versions',  () => {
